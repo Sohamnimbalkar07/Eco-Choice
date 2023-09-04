a5493decdac494c26ea339eaff92d03497d5a164
Sohamnimbalkar07 <131379333+Sohamnimbalkar07@users.noreply.github.com>
2023-09-05 02:25:04 +0530

    Delete trigger.sql

--- trigger.sql
+++
@@ -1,9 +0,0 @@
-DELIMITER $
-
-CREATE TRIGGER UpdateTotalUnits AFTER INSERT ON order_item FOR EACH ROW
-BEGIN
-    
-    update product SET total_units = total_units - NEW.quantity where id = NEW.product_id;
-END $
-
-DELIMITER ;
