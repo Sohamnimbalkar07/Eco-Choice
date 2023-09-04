0a722d678b3a59c5cdc05c9f88a063462b6ae013
Sohamnimbalkar07 <131379333+Sohamnimbalkar07@users.noreply.github.com>
2023-09-05 02:08:19 +0530

    Update trigger.sql

--- trigger.sql
+++ trigger.sql
@@ -3,9 +3,7 @@ DELIMITER $
 CREATE TRIGGER UpdateTotalUnits AFTER INSERT ON order_item FOR EACH ROW
 BEGIN
     
-    UPDATE product
-    SET total_units = total_units - NEW.quantity
-    WHERE id = NEW.product_id;
+    update product SET total_units = total_units - NEW.quantity where id = NEW.product_id;
 END $
 
 DELIMITER ;
